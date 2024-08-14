df2f35f4c44cc1aedaab897a3df4eb50f74bdf3c
kalops1a <vadymduchenko@gmail.com>
2024-08-14 17:07:10 +0200

    6

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../redux/contactsOps';
 
-const Contact = ({ contact }) => (
-  <li>
-    {contact.name}: {contact.number}
-  </li>
-);
+const Contact = ({ contact }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteContact(contact.id));
+  };
+
+  return (
+    <li>
+      {contact.name}: {contact.number}
+      <button onClick={handleDelete}>Delete</button>
+    </li>
+  );
+};
 
 Contact.propTypes = {
   contact: PropTypes.shape({
